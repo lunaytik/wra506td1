a493904fa2423ea63053d7cde6732388f81e19c7
lunaytik <nathandeuil13@gmail.com>
2023-09-19 10:28:34 +0200

    fix: add component.vue

---
+++ src/components/Component.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h2>First Component</h2>
+  </div>
+</template>
+
+<style scoped>
+  h2 {
+    color: darkred;
+    font-weight: 600;
+  }
+</style>
