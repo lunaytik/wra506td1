a7644724cb4c5e5d25d0fd7746ac3f84a35c99a8
lunaytik <nathandeuil13@gmail.com>
2023-09-19 10:26:34 +0200

    feat: exo 1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from 'vue'
+import Component from './components/Component.vue'
+
+const data = {
+  symfony: '15',
+  integration: '20',
+  reseau: '12'
+}
+
+const note1 = 15
+const note2 = 20
+const note3 = 12
+
+
+const moy = computed(() => {
+  return ((note1 + note2 + note3)/3).toFixed(2);
+})
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <ul>
+        <li v-for="(note, matiere) in data">{{ matiere.toUpperCase() }} : {{ note }}</li>
+      </ul>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <h1>Moyenne de : {{ moy }}</h1>
+      <Component></Component>
     </div>
   </header>
 
@@ -21,65 +41,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  h1 {
+    font-style: italic;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
